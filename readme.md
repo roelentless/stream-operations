376c749b36fe9b9ecf12d02705bafafdf0334c04
Roel Berger <berger.roel@gmail.com>
2017-08-19 18:55:36 +0200

    Added map, flatMap, filter, reduce, each. Added extend().

--- readme.md
+++ readme.md
@@ -19,13 +19,92 @@ var StreamOperations = require('stream-operations');
 Operations
 ----------
 
-### BufferWithTimeOrSize(time, size)
-
+### bufferWithTimeOrSize(time, size, options)
 Buffer elements in an array and flush when either time or size limit is reached.   
 Default flush after 1000ms or size 1000.
 
 ```
 aReadable
-.pipe(StreamOperations.BufferWithTimeOrSize(100, 100))  // Flush buffer after 100 milliseconds or when 100 objects are collected
-.pipe(aWritable)
+.pipe(StreamOperations.bufferWithTimeOrSize(100, 100))  // Flush buffer after 100 milliseconds or when 100 objects are collected
+...
+```
+
+### map(mapFunction, options)
+Transform an object by calling the handler for every object
+
+```
+aReadable
+.pipe(StreamOperations.map(d => d.id)) 
+...
+```
+
+### filter(filterFunction, options)
+Transform an object by calling the handler for every object
+
+```
+aReadable
+.pipe(StreamOperations.filter(d => true)) 
+...
+```
+
+### flatMap(mapFunction, options)
+Transform an object by calling the handler for every object, which can be asynchronically. Either use the callback, or return a promise.
+
+```
+aReadable
+.pipe(StreamOperations.flatMap((data, enc, done) => {
+  done(null, data)
+})) 
+.pipe(StreamOperations.flatMap(data => {
+  return new Promise((resolve, reject) => {
+    resolve(data) // or reject
+  })
+}))
+... 
+```
+
+### reduce(reduceFunction, initialValue, options)
+Reduce a stream to a single value, which is sent through the stream when the previous link finishes.
+
+```
+aReadable
+.pipe(StreamOperations.reduce((data, enc, previousValue) => previousValue++), 0) 
+...
+```
+
+### each(eachFunction, options)
+Do something for every object
+
+```
+aReadable
+.pipe(StreamOperations.each(d => console.log(d))) 
+...
+```
+
+Options
+-------
+Options can be anything provided by the Stream.Transform interface, by default: 
+```
+{ 
+  objectMode: true
+}
+```
+
+Extending
+---------
+When calling `StreamOperations.extend()` once in your code, node streams are extended with most of the stream operations.
+
+**Use this with caution!**
+
+While this is handy for speeding up your development and creates code with less clutter, don't use this in modules, since you are modifying the prototypes of streams.
+
+```
+readable
+  .map(d => d)
+  .filter(d => d)
+  .pipe(Stream.Transform)
+  .flatMap((d, enc, done) => { ... })
+  .reduce(...)
+  .each(...)
+  .on('error', err => { ... })
 ```
--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 'use strict';
 
 let Stream = require('stream');
-
 let sop = require('./index');
+
 
 let total = 0;
 let source = new Stream.Readable({
@@ -24,7 +24,7 @@ let source = new Stream.Readable({
 
 let received1 = 0;
 source
-    .pipe(sop.BufferWithTimeOrSize(10))
+    .pipe(sop.bufferWithTimeOrSize(10))
     .pipe(new Stream.Writable({
         objectMode: true,
         write:function(arr, enc, callback) {
@@ -43,7 +43,7 @@ source
 
 let received2 = 0;
 source
-    .pipe(sop.BufferWithTimeOrSize(10000, 10))
+    .pipe(sop.bufferWithTimeOrSize(10000, 10))
     .pipe(new Stream.Writable({
         objectMode: true,
         write:function(arr, enc, callback) {
@@ -58,3 +58,101 @@ source
     .on('finish', () => {
         if(total !== received2) throw 'Size buffered not in sync';
     });
+
+
+sop.extend()
+sop.extend() // Test multiple extends
+let mapped = 0;
+let flatMapped1 = 0;
+let flatMapped2 = 0;
+let filtered = 0;
+let eached = 0;
+let reduced = 0;
+let reduceResult = 0;
+source
+    .pipe(sop.map(d => {
+        mapped++;
+        return d
+    }))
+    .pipe(sop.flatMap((d, enc, done)=> {
+        setTimeout(() => {
+            flatMapped1++
+            done(null, d)
+        }, 1)
+    }))
+    .pipe(sop.flatMap(d => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                flatMapped2++
+                resolve(d)
+            }, 1)
+        })
+    }))
+    .pipe(sop.filter(d => {
+        filtered++;
+        return d;
+    }))
+    .pipe(sop.each(d => {
+        eached++;
+    }))
+    .pipe(sop.reduce((d, prev) => {
+        reduced++;
+        prev++;
+    }, 0))
+    .pipe(sop.each(d => {
+        reduceResult++;
+    }))
+    .on('finish', () => {
+        if(total != filtered || total != eached || total != reduced || total != mapped || total != flatMapped1 || total != flatMapped2 || reduceResult != 1) {
+            throw 'Size extended not correct'
+        }
+    })
+
+sop.extend()
+sop.extend() // Test double    
+let mapped2 = 0;
+let flatMapped21 = 0;
+let flatMapped22 = 0;
+let filtered2 = 0;
+let eached2 = 0;
+let reduced2 = 0;
+let reduceResult2 = 0;
+source
+    .map(d => {
+        mapped2++;
+        return d
+    })
+    .flatMap((d, enc, done)=> {
+        setTimeout(() => {
+            flatMapped21++
+            done(null, d)
+        }, 1)
+    })
+    .flatMap(d => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                flatMapped22++
+                resolve(d)
+            }, 1)
+        })
+    })
+    .filter(d => {
+        filtered2++;
+        return d;
+    })
+    .each(d => {
+        eached2++;
+    })
+    .reduce((d, prev) => {
+        reduced2++;
+        prev++;
+    }, 0)
+    .each(d => {
+        reduceResult2++;
+    })
+    .on('finish', () => {
+        if(total != filtered2 || total != eached2 || total != reduced2 || total != mapped2 || total != flatMapped21 || total != flatMapped22 || reduceResult != 1) {
+            throw 'Size extended not correct'
+        }
+    })
+
